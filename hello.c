d6c385cc4dac8f7553b3f492641de56958bfe6a7
nzetun <nzetocha2@unl.edu>
2020-08-17 19:28:07 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Noah Zetocha
+ * Date: 2020
+ *
+ * A Simple hello world program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello world!\n");
+
+    return 0;
+}
